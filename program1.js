31ebd5340f0e140c006bed2e6bf337b5528fb7b4
Sriram <sriram26112001@gmail.com>
2024-06-08 16:08:14 +0530

    example2 branch modified

--- program1.js
+++ program1.js
@@ -2,11 +2,30 @@ const express=require("express");
 const zod=require("zod");
 const app=express();
 
+<<<<<<< Updated upstream
 //now we will define the schema
 const schema=zod.object({
     email:zod.string(),
     password:zod.string(),
     country:zod.union([zod.literal("US"),zod.literal("IND")]),
+=======
+const schema=zod.object({
+    email:zod.string().email().refine(email =>
+        email.endsWith("@gmail.com"),{
+            message:"email should end with @gmail.com"
+        }
+    ),
+    password:zod.string().min(8,{
+        message:"password should be atleast 8 characaters"
+    }).refine(password =>
+        /[A-Za-z]/.test(password) &&
+        /\d/.test(password) &&
+        /[!@#$%^&*(),.?":{}|<>]/.test(password),{
+            message:"the password should be atleast 8 characters and should contain atleast one letter,one digit and one special character"
+        }
+    ),
+    country:zod.union([zod.literal("US"),zod.literal("IND")]), 
+>>>>>>> Stashed changes
     kidneys:zod.array(zod.number())
 });
 
@@ -38,4 +57,9 @@ let port=3000;
 app.listen(port,function()
 {
     console.log("server is running on http://localhost:3000");
+<<<<<<< Updated upstream
 });
+=======
+});
+
+>>>>>>> Stashed changes
