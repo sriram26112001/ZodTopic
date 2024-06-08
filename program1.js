6b03ae7c23fa2e5cc62c37bf633af074cade6254
Sriram <sriram26112001@gmail.com>
2024-06-08 13:25:06 +0530

    3rd example addded where I validate the array

--- program1.js
+++ program1.js
@@ -1,41 +1,9 @@
-const express=require("express");
 const zod=require("zod");
-const app=express();
 
-//now we will define the schema
-const schema=zod.object({
-    email:zod.string(),
-    password:zod.string(),
-    country:zod.union([zod.literal("US"),zod.literal("IND")]),
-    kidneys:zod.array(zod.number())
-});
-
-app.use(express.json());
-
-app.get("/health-checkup",function(req,res){
-    const response=schema.safeParse(req.body);
-    if(!response.success)
-    {
-        res.status(411).json({
-            message:"Input validation failed",
-            error: response.error.errors
-        });
-    }
-    else
-    {
-        const kidneys=req.body.kidneys;
-        let count=kidneys.length;
-        console.log("you have "+count+" kidneys");
-        res.json({
-            message: "you have "+count+" kidneys",
-            kidneys: kidneys
-        });
-    }
-});
-
-let port=3000;
-
-app.listen(port,function()
+function validateArray(arr)
 {
-    console.log("server is running on http://localhost:3000");
-});
+    const schema=zod.array(zod.number());
+    const response=schema.safeParse(arr);
+    console.log(response);
+}
+validateArray([1,2,3,4]);
